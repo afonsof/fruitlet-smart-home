96dc3f0baba96407674b50d332ec6b8e9129336f
Afonso F <afonso.franca@gmail.com>
2017-09-21 23:22:19 -0300

    Batch Actions

---
+++ batch.js
@@ -0,0 +1,84 @@
+const Promise = require('bluebird');
+const path = require('path');
+
+//Topics
+const place = 'home';
+
+//Rooms
+const room = {
+    livingRoom: path.join(place, 'living-room'),
+    bedroom: path.join(place, 'bedroom'),
+    kitchen: path.join(place, 'kitchen'),
+    batch: path.join(place, 'batch')
+};
+
+const device = {
+    light: 'light',
+    tv: 'tv',
+    gate: 'gate'
+};
+
+const cmnd = {
+    power: 'cmnd/power',
+    setSource: 'cmnd/set-source',
+    run: 'cmnd/run'
+};
+
+const livingRoomLightPower = path.join(room.livingRoom, device.light, cmnd.power);
+const kitchenLightPower = path.join(room.kitchen, device.light, cmnd.power);
+const bedroomLightPower = path.join(room.bedroom, device.light, cmnd.power);
+const batchRun = path.join(room.batch, cmnd.run);
+const livingRoomTvPower = path.join(room.livingRoom, device.tv, cmnd.power);
+const livingRoomTvSetSource = path.join(room.livingRoom, device.tv, cmnd.setSource);
+
+
+const payloads = {
+    on: 'ON',
+    off: 'OFF',
+    turnOffLights: 'TURN-OFF-LIGHTS',
+    netflixMode: 'NETFLIX-MODE',
+    sleepMode: 'SLEEP-MODE'
+};
+
+
+module.exports = function (client, payload) {
+    let type = payload.toString();
+
+    const options = {
+        qos: 0,
+        retain: false
+    };
+
+    switch (type) {
+        case payloads.turnOffLights:
+            return Promise.all([
+                publish(client, livingRoomLightPower, payloads.off, options),
+                publish(client, kitchenLightPower, payloads.off, options),
+                publish(client, bedroomLightPower, payloads.off, options)
+            ]);
+        case payloads.netflixMode:
+            return publish(client, batchRun, payloads.turnOffLights, options)
+                .then(()=> publish(client, livingRoomTvPower, payloads.on))
+                .delay(8000)
+                .then(()=>publish(client, livingRoomTvSetSource, 'Netflix'));
+
+        case payloads.sleepMode:
+            return Promise.all([
+                publish(client, batchRun, payloads.turnOffLights, options),
+                publish(client, livingRoomTvPower, payloads.off, options)
+            ]);
+    }
+    return Promise.resolve();
+};
+
+
+function publish(client, topic, payload, options) {
+    return new Promise(function (resolve, reject) {
+        client.publish(topic, payload, options, function (err) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve();
+        });
+    });
+}
--- index.js
+++ index.js
@@ -1,42 +1,65 @@
-
 const mosca = require('mosca');
 const mqtt = require('mqtt');
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 
+const batch = require('./batch');
 
-var mqttServer = new mosca.Server({port: 1883});
+const mqttServer = new mosca.Server({port: 1883});
 
 mqttServer.on('clientConnected', client =>
-  console.log('client connected', client.id));
+    console.log('client connected', client.id));
 
-mqttServer.on('ready', () => {
-  console.log('MQTT server is running');
-  startHttpBridge();
+mqttServer.on('published', function(packet, client) {
+    //console.log('Published', JSON.stringify(packet));
+    console.log('');
+    console.log('Published');
+    console.log('=========');
+    console.log('Topic:', packet.topic);
+    console.log('Payload:', packet.payload.toString());
 });
 
-function startHttpBridge(){
-  const app = express()
-  app.use(bodyParser.json())
+mqttServer.on('ready', () => {
+    console.log('MQTT server is running');
+    onServerReady();
+});
 
-  app.post('/publish', function (req, res) {
-      var message = {
-        topic: req.body.topic,
-        payload: req.body.message, // or a Buffer
-        qos: 2, // 0, 1, or 2
-        retain: false // or true
-      };
-      console.log('http call:')
-      console.log(JSON.stringify(message));
+function onServerReady() {
 
-      mqttServer.publish(message, function() {
-        res.status(200).send(JSON.stringify(message));
-      });
-  });
+    const client = mqtt.connect('mqtt://127.0.0.1');
 
-  app.listen(3000, function () {
-    console.log('Web Server is running');
-  });
+    client.on('connect', function () {
+        client.subscribe('home/batch/cmnd/run')
+    });
+
+    client.on('message', function (topic, payload) {
+        if (topic === 'home/batch/cmnd/run') {
+            batch(client, payload)
+                .catch(err => console.log(err));
+        }
+    });
+
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.post('/publish', function (req, res) {
+        let message = {
+            topic: req.body.topic,
+            payload: req.body.message, // or a Buffer
+            qos: 2, // 0, 1, or 2
+            retain: false // or true
+        };
+        console.log('http call:');
+        console.log(JSON.stringify(message));
+
+        mqttServer.publish(message, function () {
+            res.status(200).send(JSON.stringify(message));
+        });
+    });
+
+    app.listen(3000, function () {
+        console.log('Web Server is running');
+    });
 }
 
 
